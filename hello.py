564061befee2a7bae7463ef7db25085066910f95
{KyounghoonHan} <{khhan7813@gmail.com}>
2022-05-06 13:25:10 +0900

    Git practice
    Check 3 or 5 or 3 and 5 multiples

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 print('hello')
 
 for i in range(20):
-    if (i+1) % 3 == 0:
+    if (i+1) % 3 == 0 and (i+1) % 5 != 0:
         print('year')
-    elif (i+1) % 5 == 0:
+    elif (i+1) % 5 == 0 and (i+1) % 3 != 0:
         print('dream')
+    elif (i+1) % 5 == 0 and (i+1) % 3 == 0:
+        print('year dream') 
     else:
         print(i+1) 
