8eac6631ff73f78a3d8a74bfb7a9704923b5d443
{KyounghoonHan} <{khhan7813@gmail.com}>
2022-05-06 13:18:49 +0900

    Revised first assignment for testing git
    Check mutiples of 5

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 print('hello')
 
-for i in range(10):
+for i in range(20):
     if (i+1) % 3 == 0:
         print('year')
+    elif (i+1) % 5 == 0:
+        print('dream')
     else:
         print(i+1) 
